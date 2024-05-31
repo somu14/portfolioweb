e79aba72236055567ee359503cc647f3cf68d519
Tathagata Nayak <80235249+somu14@users.noreply.github.com>
2024-05-31 12:12:48 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function Contact() {
 
   const [words ,setwords]=useState(0);
 
+  const [email,setemail]=useState('');
+  const [subject,setsubject]=useState('');
+  const [body,setbody]=useState('');
+  const [status ,setstatus]=useState('')
+
   function wordcount(word)
   {
     setwords(word);
+  }
+  const formdata={
+    subjectdata:subject,
+    emaildata:email,
+    bodydata:body,
+  }
+  function handleclick()
+  {
+    axios.post("https://mailserver-flax.vercel.app/send",formdata)
+    .then((response)=>{console.log("requesrmade",response); set();})
+    .catch((err)=>{console.log("error is",err);})
+  }
+  function set()
+  {
+    setstatus("Thanks for Contacting you will be soon replied 😊");
+    setTimeout(() => {
+      setstatus('');
+    },3000);
   }
 
   return (
@@ -14,15 +38,33 @@ function Contact() {
      <div className="mt-9 ml-64">
         <h1 className=" text-4xl font-bold ">Hi 👋, I’m Tathagata Nayak</h1>
         <p className="mt-3 text-gray-500">Send an email and lets connect </p>
+        <p className="mt-1 text-green-600">{status}</p>
         </div>
-        <div className="bg-neutral-300  w-2/4 ml-64 m-8 rounded-lg p-4">
-            <input type='email' className="h-10 rounded-xl w-3/4 outline-none p-3 " placeholder="Exapmle@gmail.com"></input>
-            <input type='Text' className="h-10 rounded-xl w-3/4 outline-none p-3 mt-6" placeholder="Subject:lets connect"></input>
+        <div className="bg-neutral-300  w-2/4 ml-64 m-5 rounded-lg p-4">
+            <input 
+            type='email' 
+            className="h-10 rounded-xl w-3/4 outline-none p-3 " 
+            placeholder="Exapmle@gmail.com" 
+            value={email}
+            onChange={(e)=>{setemail(e.target.value)}}
+            required
+            />
+            <input 
+            type='Text' 
+            className="h-10 rounded-xl w-3/4 outline-none p-3 mt-6" 
+            placeholder="Subject:lets connect" 
+            value={subject}
+            onChange={(e)=>{setsubject(e.target.value)}}
+            />
             <div className="mt-2">
             <textarea
-            placeholder='Enter Text' className="w-3/4 p-3 rounded-lg outline-none mt-8" onChange={(e)=>{
-              const wordlen=e.target.value.length;
-              wordcount(wordlen);
+            placeholder='Enter Text' 
+            className="w-3/4 p-3 rounded-lg outline-none mt-8"
+            value={body}
+            onChange={(e)=>{
+            setbody(e.target.value);
+            const wordlen=e.target.value.length;
+            wordcount(wordlen);
             }}/>
             <h6 className="text-gray-400 flex justify-end mr-40">{words}</h6>
             </div>
@@ -30,6 +72,11 @@ function Contact() {
             hover:text-white" 
             onClick={(e)=>{
                 e.preventDefault();
+                handleclick();
+                setbody('');
+                setemail('');
+                setsubject('');
+                setwords(0);
             }}>Send
             </button>
         </div>
