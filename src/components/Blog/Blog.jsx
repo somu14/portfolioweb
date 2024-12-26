8a9e8d7a036b92bed7dbb426b8ad830b23ddeed9
Tathagata Nayak <80235249+somu14@users.noreply.github.com>
2024-12-26 23:00:48 +0530

    Update Blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Blog() {
   return (
     <>
-    <div className="ml-64 mt-9">
-        <h1 className="text-4xl">Blog Posts </h1>
-        <p className="mt-2 text-gray-400">Check out my blog posts peek into my life and  hobbies </p>
-    </div>
-    <div className="mt-6">
-        <div className="bg-neutral-200 w-2/3 ml-64 p-3 rounded-lg">
-             Hello viewers recently I have been studying japanese and preparing for the JLPT N5 
-            along with my friend and I am actively looking for an IT job in JAPAN 
-            Every day gaining little knowledge of the language .
-            wish me luck.
-            ありがとう(Thank You)
+      <div className="mt-9 px-4 sm:px-0 lg:px-16 md:ml-64">
+        <h1 className="text-3xl sm:text-4xl">Blog Posts</h1>
+        <p className="mt-2 text-gray-400">
+          Check out my blog posts for a peek into my life and hobbies
+        </p>
+      </div>
+
+      <div className="mt-6 px-4 sm:px-8 lg:px-16">
+        <div
+          className="bg-neutral-200 p-4 rounded-lg w-full sm:w-3/4 lg:w-2/3 mx-auto"
+          style={{ backgroundColor: `var(--cardcontainercolor)` }}
+        >
+          Hello viewers, recently I have been studying Japanese and preparing
+          for the JLPT N5 along with my friend, and I am actively looking for an
+          IT job in JAPAN. Every day I gain a little knowledge of the language.
+          Wish me luck!
+          <br />
+          ありがとう (Thank You)
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
