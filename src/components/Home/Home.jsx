64134312f1b9d64c60a3e9175c976fb8c28ea063
Tathagata Nayak <80235249+somu14@users.noreply.github.com>
2024-12-26 22:56:57 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,51 +1,74 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
-import githubIcon from "../../assets/github (1).png";
-import lk from "../../assets/linkedin.png";
-import ig from "../../assets/instagram.png";
-import '../../App.css'
 function Home() {
   return (
     <>
-      <div className="mt-9 ml-64">
-        <h1 className=" text-4xl font-bold ">Hi 👋, I’m Tathagata Nayak</h1>
-        <br />
-        <p className="text-1.5xl text-gray-500">
-          Hey I am your friendly developer and Computer Science
-          UnderGrad,passionate. to build useful products with
-        </p>
-        <p className=" text-gray-500 "> Technology</p>
-        <br />
-        <p className=" text-gray-500 ">
-          Here I share my journey how I work on all my projects and experience
-          if you like the content do share it with your
-        </p>
-        <p className="  text-gray-500 ">Friends</p>
-      </div>
-      <div className="mt-9 ml-64 flex">
-        <a href="https://github.com/tathagata17" target="blank">
-        <img src={githubIcon}  alt="GitHub" className="w-8 h-8 m-1" />
-        </a>
-        <a href="https://www.linkedin.com/in/tathagata-nayak-957864211/" target="blank">
-        <img src={lk} alt="linkedIn" className="w-8 h-8 m-1 ml-2" />
-        </a>
-        <a href="https://www.instagram.com/tathagata__14">
-        <img src={ig} alt="Instagram" className="w-8 h-8 m-1 ml-2" />
-        </a>
-      </div>
-      <div>
-        <p className=" text-2xl ml-64 m-4" >Skills</p>
-        <div className="bg-neutral-300 h-28 p-1 flex justify-center rounded-lg ml-64 w-2/3 overflow-x-auto">
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">C++</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">JAVA</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">JS</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">PHP</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">NODE JS</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">Mongodb</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">MYsQL</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">RESTApi</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">React</div>
-            <div className="bg-white h-14 m-2 w-28 p-3 rounded-lg flex ">ARDUINO</div>
+      <div className="md:mr-64">
+        <div className="mt-4 md:mt-4 px-4 md:px-0 mx-auto md:w-2/3 md:ml-64">
+          <h1 className="text-3xl md:text-4xl font-bold">
+            Hi 👋, I’m Tathagata Nayak
+          </h1>
+          <br />
+          <p className="text-xl md:text-lg text-gray-500">
+            Hey there! 👋 I’m a passionate developer and Computer Science
+            undergrad, always on the lookout to create meaningful tech
+            solutions.
+          </p>
+          <p className="text-lg md:text-lg text-gray-500"></p>
+          <br />
+          <p className="text-lg md:text-lg text-gray-500">
+            From coding cool projects to exploring the latest in tech, I’m here
+            to share my journey—warts and all! If you vibe with my story or find
+            something useful, feel free to spread the word with your friends.
+            Let’s build and learn together! 💡🚀
+          </p>
+          <p className="text-lg md:text-lg text-gray-500"></p>
+        </div>
+
+        <div className="mt-9 md:mt-3 flex  space-x-4 mx-auto md:w-2/3 md:ml-64">
+          <a href="https://github.com/tathagata17" target="blank">
+            <FaGithub color={`var(--primarycolor)`} size={40} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tathagata-nayak-957864211/"
+            target="blank"
+          >
+            <FaLinkedin color={`var(--primarycolor)`} size={40} />
+          </a>
+          <a href="https://www.instagram.com/tathagata__14" target="blank">
+            <FaInstagram color={`var(--primarycolor)`} size={40} />
+          </a>
+        </div>
+
+        <div className="md:ml-64">
+          <p className="text-2xl md:text-3xl mt-6 md:mt-9 md:w-full">Skills</p>
+          <div
+            className=" h-auto p-0 flex flex-wrap justify-center rounded-lg md:w-full"
+            style={{ backgroundColor: `var(--cardcontainercolor)` }}
+          >
+            {[
+              "C/C++",
+              "JAVA",
+              "JavaScript",
+              "PHP",
+              "NODE JS",
+              "MongoDB",
+              "MySQL",
+              "REST API",
+              "React JS",
+              "ARDUINO",
+              "ReactNative",
+              "Python",
+            ].map((skill) => (
+              <div
+                className="bg-white h-14 w-28 m-2 p-3 rounded-lg flex justify-center items-center text-sm md:text-base hover:bg-gray-300"
+                style={{ backgroundColor: `var(--cardcolor)` }}
+              >
+                {skill}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
